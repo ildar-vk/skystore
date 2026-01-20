702024ebddca7773d95e86a5742389fd6aef0182
ildar-vk <kalimov.ildar.v@gmail.com>
2026-01-21 01:06:29 +0500

    Complete homework 1

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+
+
+def home(request):
+    """Контроллер главной страницы"""
+    return render(request, 'catalog/home.html')
+
+
+def contacts(request):
+    """Контроллер страницы контактов"""
+    if request.method == 'POST':
+        # Получаем данные из формы
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+
+        # Здесь можно добавить логику обработки формы
+        # Например, сохранение в базу данных или отправка email
+        print(f'Новое сообщение от {name} ({phone}): {message}')
+
+    return render(request, 'catalog/contacts.html')
