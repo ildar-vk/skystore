d8774eed8fd1f6249db81c0f08cfa81128a6e300
ildar-vk <kalimov.ildar.v@gmail.com>
2026-02-17 22:33:47 +0500

    Переименование шаблонов

--- catalog/views.py
+++ catalog/views.py
@@ -1,14 +1,63 @@
+# catalog/views.py (полная версия)
 from django.views.generic import ListView, DetailView, TemplateView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
 from .models import Product, Category
-from .forms import ProductForm  # Импортируем форму
+from .forms import ProductForm
 
 
+# 🔥 ДОБАВИТЬ: Главная страница
+class IndexView(TemplateView):
+    template_name = 'catalog/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Главная'
+        return context
+
+
+# 🔥 ДОБАВИТЬ: Страница контактов
+class ContactView(TemplateView):
+    template_name = 'catalog/contact.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Контакты'
+        return context
+
+
+# 🔥 ДОБАВИТЬ: Список продуктов
+class ProductListView(ListView):
+    model = Product
+    template_name = 'catalog/product_list.html'
+    context_object_name = 'products'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        category_id = self.kwargs.get('category_id')
+        if category_id:
+            queryset = queryset.filter(category_id=category_id)
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Каталог'
+        context['categories'] = Category.objects.all()
+        return context
+
+
+# 🔥 ДОБАВИТЬ: Детальная информация о продукте
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'catalog/product_detail.html'
+    context_object_name = 'product'
+
+
+# У вас уже есть:
 class ProductCreateView(LoginRequiredMixin, CreateView):
     model = Product
-    form_class = ProductForm  # Используем нашу кастомную форму
+    form_class = ProductForm
     template_name = 'catalog/product_form.html'
 
     def get_success_url(self):
@@ -24,9 +73,10 @@ class ProductCreateView(LoginRequiredMixin, CreateView):
         return super().form_invalid(form)
 
 
+# У вас уже есть:
 class ProductUpdateView(LoginRequiredMixin, UpdateView):
     model = Product
-    form_class = ProductForm  # Используем нашу кастомную форму
+    form_class = ProductForm
     template_name = 'catalog/product_form.html'
 
     def get_success_url(self):
@@ -42,6 +92,7 @@ class ProductUpdateView(LoginRequiredMixin, UpdateView):
         return super().form_invalid(form)
 
 
+# У вас уже есть:
 class ProductDeleteView(LoginRequiredMixin, DeleteView):
     model = Product
     template_name = 'catalog/product_confirm_delete.html'
@@ -50,3 +101,15 @@ class ProductDeleteView(LoginRequiredMixin, DeleteView):
     def delete(self, request, *args, **kwargs):
         messages.success(self.request, '✅ Продукт успешно удален!')
         return super().delete(request, *args, **kwargs)
+
+
+# 🔥 ДОБАВИТЬ: Список категорий
+class CategoryListView(ListView):
+    model = Category
+    template_name = 'catalog/category_list.html'
+    context_object_name = 'categories'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Категории'
+        return context
--- config/urls.py
+++ config/urls.py
@@ -2,13 +2,18 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from catalog.debug_views import debug_catalog  # Добавьте этот импорт
+from django.contrib.auth import views as auth_views  # Добавляем этот импорт
+from catalog.debug_views import debug_catalog
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('catalog.urls')),
     path('blog/', include('blog.urls')),
-    path('debug/', debug_catalog, name='debug'),  # Добавьте эту строку
+    path('debug/', debug_catalog, name='debug'),
+
+    # Добавляем URLs для аутентификации
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
 
 if settings.DEBUG:
