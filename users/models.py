f1a9f196ba264801df4068484711111760b654e5
ildar-vk <kalimov.ildar.v@gmail.com>
2026-02-21 22:32:10 +0500

    Homework 6: Добавлена система аутентификации пользователей

--- users/models.py
+++ users/models.py
@@ -1,6 +1,40 @@
 # users/models.py
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
+
+
+class UserManager(BaseUserManager):
+    """
+    Кастомный менеджер пользователей для модели User без username
+    """
+
+    def create_user(self, email, password=None, **extra_fields):
+        """
+        Создание обычного пользователя
+        """
+        if not email:
+            raise ValueError('Email должен быть указан')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        """
+        Создание суперпользователя
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Суперпользователь должен иметь is_staff=True')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Суперпользователь должен иметь is_superuser=True')
+
+        return self.create_user(email, password, **extra_fields)
 
 
 class User(AbstractUser):
@@ -36,6 +70,9 @@ class User(AbstractUser):
         null=True,
         help_text='Укажите вашу страну'
     )
+
+    # ВАЖНО: Указываем менеджер объектов
+    objects = UserManager()
 
     # Указываем поле для авторизации
     USERNAME_FIELD = 'email'
